1b7a6a7e4b51d82a69100f36788e87e158c66e01
JanBloch <jan.bloch@noseryoung.ch>
2020-08-08 23:33:29 +0200

    Added function getData() to class Folder

--- model/folder.js
+++ model/folder.js
@@ -1,11 +1,13 @@
 const connection = require("../connection");
+const filter = require("../filter");
 
 module.exports = class Folder {
-  constructor(id, name, parentFolderId, workspace_id) {
+  show = ["id", "name", "parent_folder_id", "workspace_id"];
+  constructor(id, name, parent_folder_id, workspace_id) {
     this.data = {
       id: id,
       name: name,
-      parentFolderId: parentFolderId,
+      parent_folder_id: parent_folder_id,
       workspace_id: workspace_id,
     };
   }
@@ -17,6 +19,11 @@ module.exports = class Folder {
   getWorkspaceId() {
     return this.data.workspace_id;
   }
+
+  getData() {
+    return filter(this.data, this.show);
+  }
+
   async getChildren() {
     return new Promise((resolve, reject) => {
       connection.query(null, null, (err, res) => {});
