5848661d8a03fadfcebd2f26bc7a633fdbed8dfc
JanBloch <jan.bloch@noseryoung.ch>
2020-08-08 14:13:07 +0200

    Minor changes in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,9 +4,11 @@ const con = require("../connection");
 
 module.exports = (req, res, next) => {
   try {
+    console.log(req.headers);
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, "ASDF");
     const userId = decodedToken.userId;
+    if (isNaN(userId)) throw new Error("");
     User.get(userId, con).then((v) => {
       req.user = v;
       next();
