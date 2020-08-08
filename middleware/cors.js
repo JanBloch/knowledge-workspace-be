916c37c42e3a34912e49b314ccd032bbc6ad8ea7
JanBloch <jan.bloch@noseryoung.ch>
2020-08-08 14:13:25 +0200

    changed port number in cors middlware

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
   res.set("Access-Control-Allow-Headers", "*");
   next();
 };
