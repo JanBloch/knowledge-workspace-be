4a10a626ad4a5755e8c8b541950616765b91d73e
JanBloch <jan.bloch@noseryoung.ch>
2020-08-08 22:47:05 +0200

    Removed unused console.log in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ const con = require("../connection");
 
 module.exports = (req, res, next) => {
   try {
-    console.log(req.headers);
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, "ASDF");
     const userId = decodedToken.userId;
